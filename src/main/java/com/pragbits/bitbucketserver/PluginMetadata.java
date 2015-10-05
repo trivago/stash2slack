7e82ac5e458ca61a151815de8d123b77dd8d90e0
daniel szabo <daniel.szabo@pragbits.com>
2015-10-05 10:11:54 +0200

    fixed pluginmetadata in order to allow adding new plugin to atlassian marketplace under the existing place

--- src/main/java/com/pragbits/bitbucketserver/PluginMetadata.java
+++ src/main/java/com/pragbits/bitbucketserver/PluginMetadata.java
@@ -3,7 +3,7 @@ package com.pragbits.bitbucketserver;
 public class PluginMetadata {
 
     public static String getPluginKey() {
-        return "com.pragbits.bitbucketserver.stash2slack";
+        return "com.pragbits.stash.stash2slack";
     }
 
     public static String getCompleteModuleKey(String moduleKey) {
